6cd575b3dca12c460034405f4b2454c4ef65bc56
Will IM <vitalii.mieliet@gmail.com>
2025-04-10 16:09:56 +0300

    Do task #2

--- js/task-2.js
+++ js/task-2.js
@@ -11,6 +11,13 @@ firstArray (масив), secondArray (масив) і maxLength (число).
 В іншому випадку функція повинна повернути весь новий масив.
 */
 
+'use strict';
+
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray).slice(0, maxLength);
+  return newArray;
+}
+
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
